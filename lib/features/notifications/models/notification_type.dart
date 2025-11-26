54e61f6d63927d08fcee8ab7b6561824ef4c6168
Shadrach01 <shadrachsamuel2000@gmail.com>
2025-11-26 11:03:28 +0100

    done with notifications screen

---
+++ lib/features/notifications/models/notification_type.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+enum NotificationType { order, delivery, promo, payment }
+
+class NotificationItem {
+  final String title;
+  final String message;
+  final String time;
+  final NotificationType type;
+  final bool isRead;
+
+  NotificationItem({
+    required this.title,
+    required this.message,
+    required this.time,
+    required this.type,
+    this.isRead = false,
+  });
+}
